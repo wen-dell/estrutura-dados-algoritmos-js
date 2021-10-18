eea761c9713a288bfbabea3ed40cc9fba0129d1d
Wendell Alves <wendellmccaulay@hotmail.com>
2021-10-18 19:53:09 -0300

    Ordenando arrays

---
+++ capitulo02/ordenando-arrays.js
@@ -0,0 +1,13 @@
+let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+
+numbers.reverse();
+
+console.log(numbers);
+
+numbers.sort();
+
+console.log(numbers);
+
+numbers.sort((a, b) => a - b);
+
+console.log(numbers);
