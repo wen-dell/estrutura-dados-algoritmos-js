acfb47f7386be192d4fa0ce829f2aa1477405c20
Wendell Alves <wendellmccaulay@hotmail.com>
2021-10-18 19:40:17 -0300

    Metodo fill

---
+++ capitulo02/array-fill.js
@@ -0,0 +1,12 @@
+let numbersCopy = Array.of(1, 2, 3, 4, 5, 6);
+numbersCopy.fill(0);
+
+console.log(numbersCopy);
+
+numbersCopy.fill(2, 1);
+
+console.log(numbersCopy);
+
+numbersCopy.fill(1, 3, 5);
+
+console.log(numbersCopy);
