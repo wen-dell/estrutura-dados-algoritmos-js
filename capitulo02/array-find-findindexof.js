87b290d9659b97fcf8e946c3bd21b47e4845ad21
Wendell Alves <wendellmccaulay@hotmail.com>
2021-10-18 21:13:41 -0300

    find, finxindexof e metodos de string nos arrays

--- capitulo02/array-find-findindexof.js
+++ capitulo02/array-find-findindexof.js
@@ -5,3 +5,9 @@ const multipleOf13 = (element, index, array) => {
 
 console.log(numbers.find(multipleOf13));
 console.log(numbers.findIndex(multipleOf13));
+
+console.log(numbers.includes(15));
+console.log(numbers.includes(20));
+
+let numbers2 = [7, 6, 5, 4, 3, 2, 1];
+console.log(numbers2.includes(4, 5));
