fe1d49d8fbb1caa8c3048eba5809043fd3dce33c
Wendell Alves <wendellmccaulay@hotmail.com>
2021-10-19 22:07:59 -0300

    Class es6

---
+++ capitulo03/es6-class-stack.js
@@ -0,0 +1,49 @@
+class Stack {
+
+    constructor() {
+        this.items = [];
+    }
+    
+    push(element) {
+        this.items.push(element);
+    }
+
+    pop() {
+        return this.items.pop();
+    }
+
+    peek() {
+        return this.items[this.items.length - 1];
+    }
+
+    isEmpty() {
+        return this.items.length === 0;
+    }
+
+    size() {
+        return this.items.length;
+    }
+
+    clear() {
+        this.items = [];
+    }
+
+    print() {
+        console.log(this.items.toString()); 
+    }
+}
+
+let stack = new Stack();
+console.log(stack.isEmpty());
+stack.push(5);
+stack.push(8);
+console.log(stack.peek());
+stack.push(11);
+console.log(stack.size());
+console.log(stack.isEmpty());
+stack.push(15);
+
+stack.pop();
+stack.pop();
+console.log(stack.size());
+stack.print();
--- capitulo03/stack.js
+++ capitulo03/stack.js
@@ -20,4 +20,27 @@ function Stack() {
     this.size = function() {
         return items.length;
     };
+
+    this.clear = function() {
+        items = [];
+    };
+
+    this.print = function() {
+        console.log(items.toString()); 
+    };
 }
+
+let stack = new Stack();
+console.log(stack.isEmpty());
+stack.push(5);
+stack.push(8);
+console.log(stack.peek());
+stack.push(11);
+console.log(stack.size());
+console.log(stack.isEmpty());
+stack.push(15);
+
+stack.pop();
+stack.pop();
+console.log(stack.size());
+stack.print();
