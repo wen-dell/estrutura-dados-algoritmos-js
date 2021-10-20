493de784e5de4f0fdd09ec3088ac869816389883
Wendell Alves <wendellmccaulay@hotmail.com>
2021-10-19 21:51:50 -0300

    Espiar pilha, verificar se esta vazia e retornar tamanho

--- capitulo03/stack.js
+++ capitulo03/stack.js
@@ -8,4 +8,16 @@ function Stack() {
     this.pop = function() {
         return items.pop();
     };
+
+    this.peek = function() {
+        return items[items.length - 1];
+    };
+
+    this.isEmpty = function() {
+        return items.length === 0;
+    };
+
+    this.size = function() {
+        return items.length;
+    };
 }
