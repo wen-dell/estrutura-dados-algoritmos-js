a77da938c6a988f28b6d09173991b6b69d959e37
Wendell Alves <wendellmccaulay@hotmail.com>
2021-10-20 21:12:56 -0300

    Novos metodos: ver no inicio, verificar vazio, tamanho e print

--- capitulo04/queue.js
+++ capitulo04/queue.js
@@ -8,6 +8,22 @@ function Queue() {
     this.dequeue = function() {
         return items.shift();
     };
-    
+
+    this.front = function() {
+        return items[0];
+    };
+
+    this.isEmpty = function() {
+        return items.length === 0;
+    };
+
+    this.size = function() {
+        return items.length;
+    };
+
+    this.print = function() {
+        console.log(items.toString());
+    };
+
 
 }
