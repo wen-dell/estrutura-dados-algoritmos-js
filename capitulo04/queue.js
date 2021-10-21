a6494366107385a7eaef3600405e4de7eff1b7d0
Wendell Alves <wendellmccaulay@hotmail.com>
2021-10-20 21:16:15 -0300

    Usando a classe Queue

--- capitulo04/queue.js
+++ capitulo04/queue.js
@@ -25,5 +25,18 @@ function Queue() {
         console.log(items.toString());
     };
 
-
 }
+
+let queue = new Queue();
+console.log(queue.isEmpty());
+queue.enqueue('John');
+queue.enqueue('Jack');
+queue.enqueue('Camila');
+
+queue.print();
+console.log(queue.size());
+console.log(queue.isEmpty());
+queue.dequeue();
+queue.dequeue();
+queue.print();
+
