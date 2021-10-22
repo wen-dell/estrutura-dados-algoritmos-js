9203573c2ceb5510507dd82f3641ca6670f16000
Wendell Alves <wendellmccaulay@hotmail.com>
2021-10-22 14:32:18 -0300

    Removendo elemento na posiçao

--- capitulo05/linked-list.js
+++ capitulo05/linked-list.js
@@ -1,6 +1,6 @@
 function LinkedList() {
 
-    let Node = function(element) {
+    let Node = function (element) {
         this.element = element;
         this.next = null;
     };
@@ -8,7 +8,7 @@ function LinkedList() {
     let length = 0;
     let head = null;
 
-    this.append = function(element) {
+    this.append = function (element) {
         let node = new Node(element);
         let current;
 
@@ -27,14 +27,38 @@ function LinkedList() {
         length++;
     };
 
-    this.insert = function(position, element) {};
-    this.removeAt = function(position) {};
-    this.remove = function(element) {};
-    this.indexOf = function(element) {};
-    this.isEmpty = function() {};
-    this.size = function() {};
-    this.toString = function() {};
-    this.print = function() {};
+    this.insert = function (position, element) { };
+
+    this.removeAt = function (position) {
+        if (position > -1 && position < length) {
+            let current = head;
+            let previous;
+            let index = 0;
+
+            if (position === 0) {
+                head = current.next;
+            } else {
+                while (index++ < position) {
+                    previous = current;
+                    current = current.next;
+                }
+
+                previous.next = current.next;
+            }
+
+            length--;
+            return current.element;
+        } else {
+            return null;
+        }
+    };
+
+    this.remove = function (element) { };
+    this.indexOf = function (element) { };
+    this.isEmpty = function () { };
+    this.size = function () { };
+    this.toString = function () { };
+    this.print = function () { };
 
 }
 
