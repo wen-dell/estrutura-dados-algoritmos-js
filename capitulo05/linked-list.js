b66db39a7e0b2d90ca7967427101a6c1166990da
Wendell Alves <wendellmccaulay@hotmail.com>
2021-10-22 15:02:08 -0300

    Adicionar metodo insert

--- capitulo05/linked-list.js
+++ capitulo05/linked-list.js
@@ -27,7 +27,30 @@ function LinkedList() {
         length++;
     };
 
-    this.insert = function (position, element) { };
+    this.insert = function (position, element) {
+        if (position >= 0 && position <= length) {
+            let node = new Node(element);
+            let current = head;
+            let previous;
+            let index = 0;
+
+            if (position === 0) {
+                node.next = current;
+                head = node;
+            } else {
+                while (index++ <= position) {
+                    previous = current;
+                    current = current.next;
+                }
+                node.next = current;
+                previous.next = node;
+            }
+            length++;
+            return true;
+        } else {
+            return false;
+        }
+    };
 
     this.removeAt = function (position) {
         if (position > -1 && position < length) {
