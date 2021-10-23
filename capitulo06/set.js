14518cb39e312b8ffcf6c66a124493366f4afae8
Wendell Alves <wendellmccaulay@hotmail.com>
2021-10-23 17:54:09 -0300

    Definindo metodo hasOwnProperty

--- capitulo06/set.js
+++ capitulo06/set.js
@@ -3,7 +3,9 @@ function Set() {
 
     this.add = function(value) {};
     this.delete = function(value) {};
-    this.has = function(value) {};
+    this.has = function(value) {
+        return items.hasOwnProperty(value);
+    };
     this.clear = function() {};
     this.size = function() {};
     this.values = function() {};
