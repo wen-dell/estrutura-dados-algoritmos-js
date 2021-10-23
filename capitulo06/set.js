12a1d1e6d63e6e5803de51ca39e8174aab979b01
Wendell Alves <wendellmccaulay@hotmail.com>
2021-10-23 17:51:27 -0300

    Implementando o esqueleto da classe Set

---
+++ capitulo06/set.js
@@ -0,0 +1,11 @@
+function Set() {
+    let items = {};
+
+    this.add = function(value) {};
+    this.delete = function(value) {};
+    this.has = function(value) {};
+    this.clear = function() {};
+    this.size = function() {};
+    this.values = function() {};
+
+}
