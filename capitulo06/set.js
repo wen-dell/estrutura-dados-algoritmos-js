f4d3a047c0722689f59e9b97ee9785033fa684fe
Wendell Alves <wendellmccaulay@hotmail.com>
2021-10-23 18:47:58 -0300

    Testar conjunto

--- capitulo06/set.js
+++ capitulo06/set.js
@@ -1,4 +1,5 @@
 function Set() {
+
     let items = {};
 
     this.add = function(value) {
@@ -20,8 +21,35 @@ function Set() {
     this.has = function(value) {
         return items.hasOwnProperty(value);
     };
-    this.clear = function() {};
-    this.size = function() {};
-    this.values = function() {};
+
+    this.clear = function() {
+        items = {};
+    };
+
+    this.size = function() {
+        return Object.keys(items).length;
+    };
+
+    this.values = function() {
+        let values = [];
+        for (let i = 0, keys = Object.keys(items); i < keys.length; i++) {
+            values.push(items[keys[i]]);
+        }
+        return values;
+    };
 
 }
+
+let set = new Set();
+set.add(1);
+console.log(set.values());
+console.log(set.has(1));
+console.log(set.size());
+set.add(2);
+console.log(set.values());
+console.log(set.has(2));
+console.log(set.size());
+set.delete(1);
+console.log(set.values());
+set.delete(2);
+console.log(set.values());
