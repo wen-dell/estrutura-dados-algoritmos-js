8e02a67b1c988cbef2d0eceaf89fe675521c9dad
Wendell Alves <wendellmccaulay@hotmail.com>
2021-10-25 23:29:02 -0300

    Sondagem linear com hashtable

---
+++ capitulo07/hash-table-linear-probing.js
@@ -0,0 +1,205 @@
+function LinkedList() {
+
+    let Node = function (element) {
+        this.element = element;
+        this.next = null;
+    };
+
+    let length = 0;
+    let head = null;
+
+    this.append = function (element) {
+        let node = new Node(element);
+        let current;
+
+        if (head === null) {
+            head = node;
+        } else {
+            current = head;
+
+            while (current.next) {
+                current = current.next;
+            }
+
+            current.next = node;
+        }
+
+        length++;
+    };
+
+    this.insert = function (position, element) {
+        if (position >= 0 && position <= length) {
+            let node = new Node(element);
+            let current = head;
+            let previous;
+            let index = 0;
+
+            if (position === 0) {
+                node.next = current;
+                head = node;
+            } else {
+                while (index++ <= position) {
+                    previous = current;
+                    current = current.next;
+                }
+                node.next = current;
+                previous.next = node;
+            }
+            length++;
+            return true;
+        } else {
+            return false;
+        }
+    };
+
+    this.removeAt = function (position) {
+        if (position > -1 && position < length) {
+            let current = head;
+            let previous;
+            let index = 0;
+
+            if (position === 0) {
+                head = current.next;
+            } else {
+                while (index++ < position) {
+                    previous = current;
+                    current = current.next;
+                }
+
+                previous.next = current.next;
+            }
+
+            length--;
+            return current.element;
+        } else {
+            return null;
+        }
+    };
+
+    this.remove = function (element) { 
+        let index = this.indexOf(element);
+        return this.removeAt(index);
+    };
+    
+    this.indexOf = function (element) { 
+        let current = head;
+        let index = -1;
+
+        while (current) {
+            if (element === current.element) {
+                return ++index;
+            }
+            
+            index++;
+            current = current.next;
+        }
+
+        return -1;
+    };
+
+    this.isEmpty = function () { 
+        return length === 0;
+    };
+
+    this.size = function () { 
+        return length;
+    };
+
+    this.toString = function () { 
+        let current = head;
+        let string = '';
+
+        while (current) {
+            string += current.element + (current.next ? 'n' : n);
+            current = current.next;
+        }
+
+        return string;
+    };
+
+    this.getHead = function () { 
+        return head;
+    };
+
+}
+
+function HashTable() {
+
+    let table = [];
+
+    let loseloseHashCode = function(key) {
+        let hash = 0;
+        for (let i = 0; i < key.length; i++) {
+            hash += key.charCodeAt(i);
+        }
+        return hash % 37;
+    };
+
+    let ValuePair = function(key, value) {
+        this.key = key;
+        this.value = value;
+        this.toString = function() {
+            return '[' + this.key + ' - ' + this.value + ']';
+        }
+    }
+
+    this.put = function(key, value) {
+        let position = loseloseHashCode(key);
+        
+        if (table[position] == undefined) {
+            table[position] = new ValuePair(key, value);
+        } else {
+            let index = ++position;
+            while (table[index] != undefined) {
+                index++;
+            }
+            table[index] = new ValuePair(key, value);
+        }
+    };
+
+    this.remove = function(key) {
+        let position = loseloseHashCode(key);
+
+        if (table[position] !== undefined) {
+            if (table[position].key === key) {
+                table[position] = undefined;
+            } else {
+                let index = ++position;
+                while (table[index] !== undefined && (table[index] && table[index].key !== key)) {
+                    index++;
+                }
+                if (table[index] && table[index].key === key) {
+                    table[index] = undefined;
+                }
+            }
+        }
+    };
+
+    this.get = function(key) {
+        let position = loseloseHashCode(key);
+
+        if (table[position] !== undefined) {
+            if (table[position].key === key) {
+                return table[position].value;
+            } else {
+                let index = ++position;
+                while (table[index] !== undefined && (table[index] && table[index].key !== key)) {
+                    index++;
+                }
+                if (table[index] && table[index].key === key) {
+                    return table[index].value;
+                }
+            }
+        }
+        return undefined;
+    };
+
+    this.print = function() {
+        for (let i = 0; i < table.length; i++) {
+            if (table[i] !== undefined) {
+                console.log(i + ": " + table[i]);
+            }
+        }
+    };
+
+}
