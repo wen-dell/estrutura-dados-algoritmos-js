9eaf4824250c4c6bce66f004cbff8e89136f6b55
Wendell Alves <wendellmccaulay@hotmail.com>
2021-10-26 15:24:50 -0300

    Inserindo novo no

--- capitulo08/tree.js
+++ capitulo08/tree.js
@@ -8,7 +8,31 @@ function BinarySearchTree() {
 
     let root = null;
 
-    this.insert = function(key) {};
+    let insertNode = function(node, newNode) {
+        if (newNode.key < node.key) {
+            if (node.left === null) {
+                node.left = newNode;
+            } else {
+                insertNode(node.left, newNode);
+            }
+        } else {
+            if (node.right === null) {
+                node.right = newNode;
+            } else {
+                insertNode(node.right, newNode);
+            }
+        }
+    };
+
+    this.insert = function(key) {
+        let newNode = new Node(key);
+        if (root === null) {
+            root = newNode;
+        } else {
+            insertNode(root, newNode);
+        }
+    };
+
     this.search = function(key) {};
     this.inOrderTraverse = function() {};
     this.preOrderTraverse = function() {};
