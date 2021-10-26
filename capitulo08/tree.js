b2ae2da785cf16e9dc07dce56bc0bf0518a322ac
Wendell Alves <wendellmccaulay@hotmail.com>
2021-10-26 14:39:41 -0300

    Arvore binaria de busca

---
+++ capitulo08/tree.js
@@ -0,0 +1,20 @@
+function BinarySearchTree() {
+
+    let Node = function(key) {
+        this.key = key;
+        this.left = null;
+        this.right = null;
+    };
+
+    let root = null;
+
+    this.insert = function(key) {};
+    this.search = function(key) {};
+    this.inOrderTraverse = function() {};
+    this.preOrderTraverse = function() {};
+    this.postOrderTraverse = function() {};
+    this.min = function() {};
+    this.max = function() {};
+    this.remove = function(key) {};
+
+}
