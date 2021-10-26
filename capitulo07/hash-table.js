f4d2e15ac20677ea93b255d6cbe0108c7212cf18
Wendell Alves <wendellmccaulay@hotmail.com>
2021-10-25 21:36:47 -0300

    Implementando hash table

--- capitulo07/hash-table.js
+++ capitulo07/hash-table.js
@@ -25,3 +25,14 @@ function HashTable() {
     };
 
 }
+
+let hash = new HashTable();
+hash.put('Gandalf', 'gandalf@email.com');
+hash.put('John', 'johnsnow@email.com');
+hash.put('Tyrion', 'tyrion@email.com');
+
+console.log(hash.get('Gandalf'));
+console.log(hash.get('Loiane'));
+
+hash.remove('Gandalf');
+console.log(hash.get('Gandalf'));
