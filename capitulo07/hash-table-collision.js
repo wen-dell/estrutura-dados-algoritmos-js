fdbe0bf47551d6db0d0c7b4212897b338ee5d31b
Wendell Alves <wendellmccaulay@hotmail.com>
2021-10-25 21:43:56 -0300

    Exemplo de colisao

---
+++ capitulo07/hash-table-collision.js
@@ -0,0 +1,39 @@
+function HashTable() {
+
+    let table = [];
+
+    let loseloseHashCode = function(key) {
+        let hash = 0;
+        for (let i = 0; i < key.length; i++) {
+            hash += key.charCodeAt(i);
+        }
+        return hash % 37;
+    };
+
+    this.put = function(key, value) {
+        let position = loseloseHashCode(key);
+        console.log(position + ' - ' + key);
+        table[position] = value;
+    };
+
+    this.remove = function(key) {
+        table[loseloseHashCode(key)] = undefined;
+    };
+
+    this.get = function(key) {
+        return table[loseloseHashCode(key)];
+    };
+
+}
+
+let hash = new HashTable();
+hash.put('Gandalf', 'gandalf@email.com');
+hash.put('John', 'johnsnow@email.com');
+hash.put('Tyrion', 'tyrion@email.com');
+hash.put('Aaron', 'aaron@email.com');
+hash.put('Donnie', 'donnie@email.com');
+hash.put('Ana', 'ana@email.com');
+hash.put('Jonathan', 'jonathan@email.com');
+hash.put('Jamie', 'jamie@email.com');
+hash.put('Sue', 'sue@email.com');
+hash.put('Mindy', 'mindy@email.com');
