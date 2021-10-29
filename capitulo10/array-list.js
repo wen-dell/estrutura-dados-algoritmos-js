1a00679a52fae8adbd2140e8976c64b8db7a94b5
Wendell Alves <wendellmccaulay@hotmail.com>
2021-10-29 19:05:51 -0300

    Bubble sort melhorado

--- capitulo10/array-list.js
+++ capitulo10/array-list.js
@@ -26,3 +26,16 @@ function ArrayList() {
         }
     };
 }
+
+function createNonSortedArray(size) {
+    let array = new ArrayList();
+    for (let i = size; i > 0; i--){
+        array.insert(i);
+    }
+    return array;
+}
+
+let array = createNonSortedArray(5);
+console.log(array.toString());
+array.bubbleSort();
+console.log(array.toString());
