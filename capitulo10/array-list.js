c8f66e5e45d2e6984bbf1ab9104350033329f3c7
Wendell Alves <wendellmccaulay@hotmail.com>
2021-10-29 19:07:52 -0300

    Bubble sort melhorado

--- capitulo10/array-list.js
+++ capitulo10/array-list.js
@@ -18,7 +18,7 @@ function ArrayList() {
     this.bubbleSort = function() {
         let length = array.length;
         for (let i = 0; i < length; i++) {
-            for (let j = 0; j < length - 1; j++) {
+            for (let j = 0; j < length - 1 - i; j++) {
                 if (array[j] > array[j+1]) {
                     swap(array, j, j+1);
                 }
