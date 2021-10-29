6944e9c3b8dcbfe4d891d2ab3942e1aa4569b7fa
Wendell Alves <wendellmccaulay@hotmail.com>
2021-10-29 20:47:11 -0300

    Algoritmo insertion sort

--- capitulo10/array-list.js
+++ capitulo10/array-list.js
@@ -41,6 +41,22 @@ function ArrayList() {
             }    
         }
     };
+
+    this.insertionSort = function() {
+        let length = array.length;
+        let j;
+        let temp;
+        for (let i = 1; i < length; i++) {
+            j = i;
+            temp = array[i];
+            while (j > 0 && array[j-1] > temp) {
+                array[j] = array[j-1];
+                j--;
+            }
+            array[j] = temp;
+        }
+    };
+
 }
 
 function createNonSortedArray(size) {
