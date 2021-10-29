a0644e12b4e70363c9478004d66eed83b0705322
Wendell Alves <wendellmccaulay@hotmail.com>
2021-10-29 19:00:34 -0300

    Bubble sort

--- capitulo10/array-list.js
+++ capitulo10/array-list.js
@@ -1,11 +1,28 @@
 function ArrayList() {
     let array = [];
 
+    let swap = function(array, index1, index2) {
+        let aux = array[index1];
+        array[index1] = array[index2];
+        array[index2] = aux;
+    };
+
     this.insert = function(item) {
         array.push(item);
     };
 
     this.toString = function() {
         return array.join();
+    };
+
+    this.bubbleSort = function() {
+        let length = array.length;
+        for (let i = 0; i < length; i++) {
+            for (let j = 0; j < length - 1; j++) {
+                if (array[j] > array[j+1]) {
+                    swap(array, j, j+1);
+                }
+            }
+        }
     };
 }
